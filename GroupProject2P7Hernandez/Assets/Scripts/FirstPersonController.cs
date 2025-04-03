78819fe9c76e994b64b544dcbce2d541f4257ff9
718355 <718355@student.dusd.net>
2025-04-03 15:27:55 -0700

    Level Design and first person script.

--- GroupProject2P7Hernandez/Assets/Scripts/FirstPersonController.cs
+++ GroupProject2P7Hernandez/Assets/Scripts/FirstPersonController.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour
 {
+    private float _speed;
+    private float _rotationVelocity;
+    private float _verticalVelocity;
+    private float _terminalVelocity = 50.0f;
 
+    private float _jumpTimeoutDelta;
+    private float _fallTimeoutDelta;
 
-    void Start()
+    private GameObject _mainCamera;
+    private void Awake()
     {
-        
-    }
-
-
-    void Update()
-    {
-        
+        if(_mainCamera == null)
+        {
+            _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        }
     }
 }
