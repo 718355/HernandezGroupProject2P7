ef94266e7d5668b43ed91351775f1e9e8c8de9bd
718355 <718355@student.dusd.net>
2025-04-07 10:19:17 -0700

    Tried to do more code.

---
+++ GroupProject2P7Hernandez/Assets/Scripts/Movement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    
+    
+}
