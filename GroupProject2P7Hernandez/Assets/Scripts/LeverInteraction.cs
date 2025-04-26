18d2ecf91d2b0bd3761a97bc609c68d3a957999e
718355 <718355@student.dusd.net>
2025-04-26 13:38:55 -0700

    Stamina

---
+++ GroupProject2P7Hernandez/Assets/Scripts/LeverInteraction.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverInteraction : MonoBehaviour
+{
+    public GameObject pressEImage;
+    public Transform gate;
+    public float gateOpenHeight = 5f;
+    public float openSpeed = 5f;
+
+    private bool isPlayerNear = false;
+    private bool gateOpened = false;
+    private Vector3 initialGatePosition;
+
+
+
+    private void Start()
+    {
+        if (pressEImage != null)
+        {
+            pressEImage.SetActive(false);
+        }
+
+        if (gate != null)
+        {
+            initialGatePosition = gate.position;
+        }
+    }
+
+    
+    private void Update()
+    {
+        if (isPlayerNear && !gateOpened && Input.GetKeyDown(KeyCode.E))
+        {
+            gateOpened = true;
+            if (pressEImage != null)
+                pressEImage.SetActive(false);
+        }
+        if(gateOpened && gate != null)
+        {
+            Vector3 targetPosition = initialGatePosition + new Vector3(0, gateOpenHeight, 0);
+            gate.position = Vector3.MoveTowards(gate.position, targetPosition, openSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+            if (pressEImage != null)
+                pressEImage.SetActive(false);
+        }
+    }
+}
